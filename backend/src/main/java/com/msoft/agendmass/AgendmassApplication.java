f4ec2dc0d3ac57f65c292e4c4e9db01d61971415
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-03-12 22:30:52 -0400

    Project created

---
+++ backend/src/main/java/com/msoft/agendmass/AgendmassApplication.java
@@ -0,0 +1,13 @@
+package com.msoft.agendmass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendmassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendmassApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/msoft/agendmass/AgendmassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msoft.agendmass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendmassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
